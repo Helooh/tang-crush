637c6c7b6487804e4719e5ceb60ea199cf2834e9
Helooh <86764660+Helooh@users.noreply.github.com>
2021-07-01 14:08:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Eicon lợn!',
+    introDesc: `hỏi câu này nháaaaaaaaaaaaaaaaa`,
+    btnIntro: 'hehe',
+    title: 'm có yêu t khumm😙',
+    desc: 'thách m bấm no đó con lợn',
+    btnYes: 'iu bạn Dũng lắm nhó:333',
+    btnNo: 'không yêu chi 3 đồ nớ ',
+    question:'Nếu được chọn lại thì m có chọn yêu t không=)))',
+    btnReply: 'send to my love:))',
+    reply: 'có................có.............và có=)))',
+    mess: 'T biết m iu t mà:))🥰. Yêu bạn 😘😘',
+    messDesc: 'Rứa thôi hết rồi:vvv',
+    btnAccept: 'byeeeee',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
